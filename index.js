55c6983773f5106ab7ab5afb19bb3c09b84294df
Dat <datnnph24820@fpt.edu.vn>
2023-05-13 19:39:19 +0700

    giao dien sever

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const  { engine } =require ('express-handlebars');
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+
+
+app.use(express.static(path.join(__dirname, 'public/img')));
+app.set('views', path.join(__dirname, 'views'));
 app.get('/', function (req, res) {
   res.render('trangchu')
 })
